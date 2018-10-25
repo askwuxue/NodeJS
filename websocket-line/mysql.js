3149dc65c1f28fb6489cb876c8bfda2ccc8afc7d
askwuxue <972523419@qq.com>
2018-10-25 19:38:33 +0800

    搭建一个聊天室

---
+++ websocket-line/mysql.js
@@ -0,0 +1,25 @@
+const mysql = require('mysql');
+
+// let db = mysql.createConnection({
+//     host: 'localhost',
+//     port: 3306,
+//     user: 'root',
+//     password: '1314995'
+// })
+
+// 连接池，默认是十个连接
+let db = mysql.createPool({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '1314995',
+    database: 'websocket'
+})
+
+db.query('SELECT * from user_table', (err, data) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(data);
+    }
+});
