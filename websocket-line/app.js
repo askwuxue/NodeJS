64efcde27e2ad21dd145871eb3c90e87c032762a
askwuxue <972523419@qq.com>
2018-10-26 22:09:46 +0800

    消息推送的问题

--- websocket-line/app.js
+++ websocket-line/app.js
@@ -10,11 +10,17 @@ let httpServer = http.createServer((request, response) => {
 let wsServer = io.listen(httpServer);
 
 wsServer.on('connection', socket => {
+    // 用户的唯一身份标识符
+    let current_user_server = Math.random().toString();
+    // console.log(current_user_server);
     // 登录
     socket.on('login', (userInfo) => {
         // console.log(userInfo);
         let username = userInfo[0];
         let password = userInfo[1];
+        console.log(username, password);
+        // 用户在线状态
+        let uset_log_status = 0;
         if (username == '' || password == '') {
             // 1. 验证信息
             socket.emit('log_ret', {
@@ -25,7 +31,7 @@ wsServer.on('connection', socket => {
             return;
         }
         // 2. 查询数据库查看是否存在用户
-        let user_Info = db.query(`SELECT * FROM user_table WHERE username = 'askwuxue'`, (error, data) => {
+        let user_Info = db.query(`SELECT * FROM user_table WHERE username = '${username}'`, (error, data) => {
             if (error) {
                 socket.emit('log_ret', {
                     "status": 0,
@@ -40,29 +46,112 @@ wsServer.on('connection', socket => {
                     })
                     // 验证密码是否正确
                 } else {
+                    // console.log('wuxue=============' + data[0].username, data[0].password)
                     if (password !== data[0].password) {
                         socket.emit('log_ret', {
                             "status": 0,
                             "msg": "用户名或者密码不正确"
                         })
                     } else {
+                        // 更新用户的状态到数据库中
+                        db.query(`UPDATE user_table SET online = 1 WHERE username = '${username}'`, (error, data) => {
+                            if (error) {
+                                console.log(error);
+                            } else {
+                                // console.log(data);
+                            }
+                        })
                         socket.emit('log_ret', {
                             "status": 1,
-                            "msg": "登录成功"
+                            "msg": "登录成功",
+                            "random": current_user_server
                         })
                     }
                 }
             }
         });
+    })
 
-
-
-
-
-        // console.log(user_Info);
-        if (user_Info) {
-
+    // 注册
+    socket.on('reg', (user_Info) => {
+        let username = user_Info[0];
+        let password = user_Info[1];
+        // 1. 验证   正则表达式
+        if (username == '' || password == '') {
+            socket.emit('reg_ret', {
+                "status": 0,
+                "msg": "用户名或者密码不合法"
+            })
+            return;
         }
+        // 2. 数据库验证
+        db.query(`SELECT username FROM user_table WHERE username = '${username}'`, (error, data) => {
+            if (error) {
+                socket.emit('reg_ret', {
+                    "status": 0,
+                    "msg": "服务器繁忙,请稍后再试..."
+                })
+            } else {
+                // 判断是否存在用户
+                if (data.length) {
+                    socket.emit('reg_ret', {
+                        "status": 0,
+                        "msg": "用户名已存在"
+                    })
+                    // 写入数据库
+                } else {
+                    db.query(`INSERT INTO user_table (username, password) VALUES ('${username}', '${password}')`, (error, data) => {
+                        if (error) {
+                            socket.emit('reg_ret', {
+                                "status": 0,
+                                "msg": "服务器繁忙,请稍后再试...."
+                            })
+                        } else {
+                            socket.emit('reg_ret', {
+                                "status": 1,
+                                "msg": "注册成功"
+                            })
+                        }
+                    })
+                }
+            }
+        });
+
+    })
+
+    // 发表
+    socket.on('speak', (speak) => {
+        // console.log(speak[0]);
+        let current_user_client = speak[0];
+        // 判断用户登陆状态
+        if (current_user_client !== current_user_server) {
+            socket.emit('speak_ret', {
+                "status": 0,
+                "msg": "登陆后才能发表留言哦..."
+            })
+            return;
+        }
+        // 验证是否为空
+        let text = speak[1];
+        if (text === '') {
+            socket.emit('speak_ret', {
+                "status": 0,
+                "msg": "发表的内容不能为空哦...."
+            })
+            return;
+        }
+        // 发表成功
+        socket.emit('speak_ret', {
+            "status": 1,
+            "msg": "发表成功"
+        })
+
+        // 推送留言
+        socket.emit('receive_msg', {
+            "status": 1,
+            "msg": text,
+            "current_user": current_user_server
+        })
         
     })
 })
@@ -70,13 +159,3 @@ wsServer.on('connection', socket => {
 httpServer.listen(3000, () => {
     console.log('server is running port 3000....');
 })
-
-
-
-// db.query('SELECT * from user_table', (err, data) => {
-//     if (err) {
-//         console.log(err);
-//     } else {
-//         console.log(data);
-//     }
-// });
